84f5efa8e1721b35e80961455df836367bc8dfa1
Scofig <127475992+Scofig@users.noreply.github.com>
2023-03-29 20:02:49 +0500

    Add files via upload

---
+++ src/Block.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let bgcolor = "black";
+    export let opacity = "1";
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div style="background-color: {bgcolor}; opacity: {opacity}" on:click></div>
+
+
+<style>
+    div {
+        width: 100px;
+        height: 100px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Block from '../Block.svelte';
+	let colors = [
+		{ bgcolor: 'red', opacity: 1 },
+		{ bgcolor: 'blue', opacity: 1 },
+		{ bgcolor: 'yellow', opacity: 1 },
+		{ bgcolor: 'black', opacity: 1 },
+		{ bgcolor: 'green', opacity: 1 }
+	];
+</script>
+
+{#each colors as { bgcolor, opacity }}
+	<Block
+		{bgcolor}
+		{opacity}
+		on:click={() => {
+			if (opacity) {
+				colors.forEach((color) => (color.opacity = 1));
+				opacity = 0;
+			}
+		}}
+	/>
+{/each}
